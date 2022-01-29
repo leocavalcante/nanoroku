912c98c6d0afbf6fdc886355881f41337c63258a
leocavalcante <lc@leocavalcante.com>
2022-01-28 22:48:07 -0300

    chore(app): hello world

--- app.php
+++ app.php
@@ -1,1 +1,9 @@
 <?php declare(strict_types=1);
+
+use Hyperf\Nano\Factory\AppFactory;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$app = AppFactory::create('0.0.0.0', (int) (getenv('PORT') ?: 9501));
+$app->get('/', fn(): string => 'Hello, World!');
+$app->run();
